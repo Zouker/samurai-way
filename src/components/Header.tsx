fe1caa92b9b952f0e575e53767ab8a18d2731722
Denis Shubin <denis.shubin@gmail.com>
2022-03-02 12:24:09 +0300

    commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src={'https://cdn3.iconfinder.com/data/icons/3d-applications/256/app_icons_social_media_search___logo_google_engine_software.png'}/>
+        </header>
+    );
+};
+
+export default Header;
